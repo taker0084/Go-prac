7d58726e4fa6be733bb0acff1ef9e4653c8b487b
Takeru Seki <0084takeru@gmail.com>
2025-01-12 18:01:05 +0900

    エラー修正

--- test/main.go
+++ test/main.go
@@ -18,7 +18,7 @@ func divide(x,y float32) (float32, error){
 	var result float32
 
 	if y==0{
-		return result, errors.New("Cannot divide by 0")
+		return result, errors.New("cannot divide by 0")
 	}
 	result=x/y
 	return result,nil
